782dc24dacf0094e9ee67550ae33be718acbfa69
Irene Dubrovsky <69083126+NovaSur@users.noreply.github.com>
2021-03-22 17:25:09 -0600

    Create paneles.js

---
+++ paneles.js
@@ -0,0 +1,8 @@
+function setVisibility(id, visibility) {
+document.getElementById(id).style.display =  visibility;
+}
+
+function writeText(txt)
+{ document.getElementById("inp").style.display =  "inline";
+document.getElementById("inp").innerHTML=txt;
+}
